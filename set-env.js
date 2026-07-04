039f5747a4d3477b2be3b66688420dd605d00fc2
0marAmara <onan383@outlook.com>
2026-07-04 09:43:32 +0300

    Use production backend URL

--- set-env.js
+++ set-env.js
@@ -3,11 +3,10 @@ const fs = require('fs');
 const targetPath = './src/environments/environment.prod.ts';
 const envConfigFile = `export const environment = {
   production: true,
-  apiUrl: '${process.env.BACKEND_URL || 'http://localhost:8080'}',
+  apiUrl: '${process.env.BACKEND_URL || 'https://smartcompare-backend-production-527d.up.railway.app'}',
   useMockAdminApi: false,
 };
 `;
 
 fs.writeFileSync(targetPath, envConfigFile);
 console.log('Environment variables set for production build.');
-
